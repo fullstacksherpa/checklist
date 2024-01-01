772d52e4190fe970de4ba76290d91a4decf6461e
fullstacksherpa <fullstacksherpa@gmail.com>
2023-12-31 19:37:02 -0800

    build: implement ContextProvider and NextTopLoader

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,23 +1,23 @@
-interface ButtonProps {
-  icon?: React.ReactNode;
-  name?: string;
-  background?: string;
-  padding?: string;
-  borderRad?: string;
-  fw?: string;
-  fs?: string;
-  click?: () => void;
-  type?: 'submit' | 'button' | 'reset' | undefined;
-  border?: string;
-  color?: string;
-}
+// interface ButtonProps {
+//   icon?: React.ReactNode;
+//   name?: string;
+//   background?: string;
+//   padding?: string;
+//   borderRad?: string;
+//   fw?: string;
+//   fs?: string;
+//   click?: () => void;
+//   type?: 'submit' | 'button' | 'reset' | undefined;
+//   border?: string;
+//   color?: string;
+// }
 
-const Button: React.FC<ButtonProps> = ({ sampleTextProp }) => {
-  return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
-      {sampleTextProp}
-    </div>
-  );
-};
+// const Button: React.FC<ButtonProps> = ({  }) => {
+//   return (
+//     <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
+//       {sampleTextProp}
+//     </div>
+//   );
+// };
 
-export default Button;
+// export default Button;
